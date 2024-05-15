b0a71258976c162e37e8881afdec2339e2557f13
mariam <maryamjokar9485@gmail.com>
2024-05-15 18:39:55 +0430

    added main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.n = 0
+
+    def get(self, ind):
+        
+
+    def insert_after(self, x, data):
+        
+    def delete(self, x):
+        
+    def find(self, val):
+
+    def size(self):
+        return self.n
+
+    def is_empty(self):
+        return self.n == 0
+
+
+
